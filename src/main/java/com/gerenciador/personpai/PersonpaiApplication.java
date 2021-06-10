d86bdd71a1a24ab0d47d0e061b0f53a8d81dbc46
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-06-10 01:32:00 -0300

    first commit

---
+++ src/main/java/com/gerenciador/personpai/PersonpaiApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.personpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonpaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonpaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciador/personpai/PersonpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.personpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonpaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
