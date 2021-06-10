96fd437b64b739676f94f62ede78d62837b654a3
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-06-10 02:21:09 -0300

    add. new entity classes person, phone and enum phone

---
+++ src/main/java/com/gerenciador/personpai/entity/Person.java
@@ -0,0 +1,21 @@
+package com.gerenciador.personpai.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private List<Phone> phones;
+}
